bc03584ceb7b561df3d2e9951dbe12d896271afb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-24 18:03:01 -0800

    Add todo notes

---
+++ src/containers/pages/cellIndexPage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class CellIndexPage extends Component {
+  render() {
+    return (
+      <div>the list of cells</div>
+      // TODO: lets use reactTable for this one
+      // TODO: filter box
+      // TODO: sort list by columns
+      // TODO: new cell button
+    );
+  }
+}
+
+/* 
+TODO: columns:
+color
+title
+% complete
+# of open tasks
+priority
+x coordinate
+y coordinate
+assign region
+user
+
+**This cell list component should be generic enough to reuse for other areas, pass in cells as prop**
+*/
