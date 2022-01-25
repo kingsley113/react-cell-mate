f242299a6198e398bcf97f869e7c9b439bd52251
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-24 16:39:52 -0800

    Create cell map page component

---
+++ src/containers/pages/cellMapPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class CellMapPage extends Component {
+  render() {
+    return <div>this will be the cell map page</div>;
+    // TODO: will need a header, selector for color mode, and the map, and show color scale and user legend when on the respective modes
+  }
+}
+
+export default CellMapPage;
