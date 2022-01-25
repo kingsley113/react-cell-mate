61bac3910136508d1597941c58d45f6236b2b0ed
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-24 21:43:50 -0800

    Create user account page

---
+++ src/containers/pages/userPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class UserPage extends Component {
+  render() {
+    return <div>this will be the user account page</div>;
+  }
+}
+
+// TODO: password reset
+// TODO: admin panel - show if user has admin rights
+// TODO: admin panel - delete users
