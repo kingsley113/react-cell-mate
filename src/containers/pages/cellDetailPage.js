2729fe87dc9e34c4f4420bfc3cf138eef3a7c379
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-24 17:21:39 -0800

    Create cell detail page component

---
+++ src/containers/pages/cellDetailPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CellDetailPage extends Component {
+  render() {
+    return <div>this will be the cell detail page</div>;
+    // TODO: cell title
+    // TODO: cell details panel
+    // TODO: cell todo panel
+    // TODO: cell comments
+    // TODO: cell quests list
+    // TODO: edit button
+    // TODO: return to index button
+    // TODO: url should be slug of title
+  }
+}
