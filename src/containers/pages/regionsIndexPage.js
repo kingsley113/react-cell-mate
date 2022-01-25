8a9b0654000ec10174448a742be0d7a364c9d5f9
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-24 18:07:31 -0800

    Create region index page

---
+++ src/containers/pages/regionsIndexPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class RegionIndexPage extends Component {
+  render() {
+    return (
+      <div>this will be the regions index page</div>
+      // TODO: use react table for this list?
+      // TODO: add region button
+    );
+  }
+}
+
+/*
+// TODO: columns
+region title
+number of cells
+% complete based on average of cells % complete 
+*/
