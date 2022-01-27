4e02222939a9a2238c32da5d1d0acbe003fa9559
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-27 14:53:10 -0800

    Flush out page header with bootstrap

--- src/containers/general/pageHeader.js
+++ src/containers/general/pageHeader.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import { Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import Logo from "../../components/header/logo";
 
 class PageHeader extends Component {
   render() {
-    return <div id="page-header">page header here</div>;
+    return (
+      <div id="page-header">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <Logo />
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-center">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/map">Cell Map</Nav.Link>
+            <Nav.Link href="/cellList">Cell List</Nav.Link>
+            <Nav.Link href="/quests">Quests</Nav.Link>
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="/user">Vault Boy</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
 
