2a29490522e943277ffbbb939be9afdba4b75d65
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-27 15:50:43 -0800

    create redirectIfLoggedIn component

---
+++ src/components/auth/redirectIfLoggedIn.js
@@ -0,0 +1,20 @@
+import { propTypes } from "react-bootstrap/esm/Image";
+import { connect } from "react-redux";
+import { Redirect } from "react-router";
+
+const RedirectIfLoggedIn = (props) => {
+  // if jwt is present then return a redirect object to home
+  if (localStorage.getItem("jwt") || props.loggedIn) {
+    return <Redirect to="/" />;
+  } else {
+    return props.children;
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.redirectToDashboard,
+  };
+};
+
+export default connect(mapStateToProps)(RedirectIfLoggedIn);
