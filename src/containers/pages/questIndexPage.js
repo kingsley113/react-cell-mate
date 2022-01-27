9c64602f4c7e9b9ff9ab652e7344dc6968b7e67b
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-27 15:21:22 -0800

    Create quest index page

---
+++ src/containers/pages/questIndexPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class QuestIndexPage extends Component {
+  render() {
+    return <div>quests page content</div>;
+  }
+}
+
+export default QuestIndexPage;
+
+// TODO:
