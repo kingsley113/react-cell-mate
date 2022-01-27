d2aabf0ad2cb29729bc2c221e2f78323b867c888
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-27 15:22:11 -0800

    Add routes to pageContent file

--- src/containers/pageContent.js
+++ src/containers/pageContent.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import PageHeader from "./general/pageHeader";
 import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import DashboardPage from "./pages/dashboardPage";
+import CellMapPage from "./pages/cellMapPage";
+import CellIndexPage from "./pages/cellIndexPage";
+import QuestIndexPage from "./pages/questIndexPage";
+import UserPage from "./pages/userPage";
 
 class PageContent extends Component {
   render() {
     return (
       <div>
-        <PageHeader />
-        page content stuff here
+        <div>
+          <PageHeader />
+          page content stuff here
+        </div>
+        <div className="page-main">
+          <Switch>
+            <Route exact path="/" render={() => <DashboardPage />} />
+            <Route exact path="/map" render={() => <CellMapPage />} />
+            <Route exact path="/cells" render={() => <CellIndexPage />} />
+            <Route exact path="/quests" render={() => <QuestIndexPage />} />
+            <Route exact path="/user" render={() => <UserPage />} />
+          </Switch>
+        </div>
       </div>
     );
   }
