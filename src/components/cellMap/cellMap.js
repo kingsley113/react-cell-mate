a9370a2cd1b92e2b37d1328bd6502291e186f8c0
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-29 11:27:04 -0800

    Create cell map component and add notes

---
+++ src/components/cellMap/cellMap.js
@@ -0,0 +1,11 @@
+const CellMap = (props) => {
+  return <div>cell map here</div>;
+};
+
+export default CellMap;
+
+// TODO: set up grid layout
+// TODO: get background image
+// TODO: make map responsive to size?
+// TODO: add tooltips on hover to each cell
+// TODO: add legend for each color mode, this should be on cell map page?
--- src/containers/pages/cellMapPage.js
+++ src/containers/pages/cellMapPage.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import CellMap from "../../components/cellMap/cellMap";
 
 class CellMapPage extends Component {
   render() {
-    return <div>this will be the cell map page</div>;
+    return (
+      <div>
+        this will be the cell map page
+        <CellMap />
+      </div>
+    );
     // TODO: will need a header, selector for color mode, and the map, and show color scale and user legend when on the respective modes
   }
 }
@@ -10,3 +16,4 @@ class CellMapPage extends Component {
 export default CellMapPage;
 
 // TODO: use offCanvas panel for legend on map
+// TODO: color mode selection, pass in prop to CellMap component for color mode
