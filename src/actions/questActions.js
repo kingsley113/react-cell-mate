479ac3735a3790febead938e03bf79b98263151e
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-01 19:52:36 -0800

    Add load quest action

--- src/actions/questActions.js
+++ src/actions/questActions.js
@@ -25,6 +25,25 @@ export const createQuest = (questObject) => {
   };
 };
 // Load quest
+export const loadQuest = (id) => {
+  return (dispatch) => {
+    const configurationObject = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("cellMateJWT")}`,
+      },
+    };
+    fetch(`${apiURL}/api/v1/quests/${id}`, configurationObject)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        dispatch({ type: "LOAD_QUEST", quest: json.quest });
+      });
+  };
+};
+
+// Load quests
 export const loadQuests = () => {
   return (dispatch) => {
     const configurationObject = {
