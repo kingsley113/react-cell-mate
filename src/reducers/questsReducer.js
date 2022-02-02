2dc2316e2cc5c7f27756d87b6c1a0c9785089cc1
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-01 19:54:08 -0800

    Update quest reducer

--- src/reducers/questsReducer.js
+++ src/reducers/questsReducer.js
@@ -1,9 +1,15 @@
 function quests(state = { quests: [] }, action) {
   switch (action.type) {
     case "ADD_QUEST":
+    // TODO:
+    case "LOAD_QUEST":
+      return { currentQuest: action.quest };
     case "LOAD_QUESTS":
+      return { allQuests: action.quests };
     case "EDIT_QUEST":
+    // TODO:
     case "DELETE_QUEST":
+    // TODO:
     default:
       return state;
   }
