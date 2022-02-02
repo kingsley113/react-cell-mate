2da08e514a5746717d3cede8d192e0f08f318e9b
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-01 18:24:59 -0800

    Build out quest index page

--- src/containers/pages/questIndexPage.js
+++ src/containers/pages/questIndexPage.js
@@ -1,12 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadQuests } from "../../actions/questActions";
+import LoadingSpinner from "../../components/general/loadingSpinner";
+import QuestTable from "../../components/quests/questTable";
 
-class QuestIndexPage extends Component {
-  render() {
-    return <div>quests page content</div>;
+const QuestIndexPage = () => {
+  const dispatch = useDispatch();
+  const quests = useSelector((state) => state.quests.allQuests);
+
+  useEffect(() => {
+    dispatch(loadQuests());
+  }, []);
+
+  if (quests) {
+    return (
+      <div>
+        quests page content
+        <QuestTable quests={quests} />
+      </div>
+    );
+  } else {
+    return <LoadingSpinner />;
   }
-}
+};
 
 export default QuestIndexPage;
 
-// TODO:
 // TODO: load quests
