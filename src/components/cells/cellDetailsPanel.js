012a8aa26721d34efcf575e557bfca99728645d5
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-01 17:45:31 -0800

    Create cell detail panel component and add to details page

---
+++ src/components/cells/cellDetailsPanel.js
@@ -0,0 +1,26 @@
+import { ListGroup, ProgressBar } from "react-bootstrap";
+
+const CellDetailsPanel = ({ cell }) => {
+  return (
+    <ListGroup id="cell-details-panel" className="panel-border">
+      <ListGroup.Item>Description: {cell.description}</ListGroup.Item>
+      <ListGroup.Item>X Coord: {cell.ck_coordinate_x}</ListGroup.Item>
+      <ListGroup.Item>Y Coord: {cell.ck_coordinate_y}</ListGroup.Item>
+      <ListGroup.Item>
+        <ProgressBar
+          now={cell.percent_complete}
+          label={`${cell.percent_complete}% Complete`}
+        />
+      </ListGroup.Item>
+      <ListGroup.Item>Priority: {cell.priority}</ListGroup.Item>
+      <ListGroup.Item>Region: {cell.region_id}</ListGroup.Item>
+      <ListGroup.Item>User: {cell.user_id}</ListGroup.Item>
+      <ListGroup.Item>Worldspace: {cell.worldspace_id}</ListGroup.Item>
+    </ListGroup>
+  );
+};
+
+export default CellDetailsPanel;
+
+// TODO: load tasks from backend
+// TODO: load quests from redux
--- src/containers/pages/cellDetailPage.js
+++ src/containers/pages/cellDetailPage.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import CellDetailsPanel from "../../components/cells/cellDetailsPanel";
 
-class CellDetailPage extends Component {
-  render() {
-    return <div>this will be the cell detail page</div>;
-    // TODO: cell title
+const CellDetailPage = (props) => {
+  // console.log(props.match.params.id);
+  if (props.cells) {
+    const cell = props.cells.filter((cell) => {
+      // console.log(cell.id);
+      return cell.id === Number(props.match.params.id);
+    })[0];
+    console.log(cell);
+
+    return (
+      <div>
+        <h1>{cell.name}</h1>
+        <CellDetailsPanel cell={cell} />
+      </div>
+    );
     // TODO: cell details panel
     // TODO: cell todo panel
     // TODO: cell comments
@@ -11,5 +23,9 @@ class CellDetailPage extends Component {
     // TODO: edit button
     // TODO: return to index button
     // TODO: url should be slug of title
+  } else {
   }
-}
+  return <h2>Loading...</h2>;
+};
+
+export default CellDetailPage;
