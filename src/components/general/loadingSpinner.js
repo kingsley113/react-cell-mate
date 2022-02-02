25de26e5fde23d922ab861b156aad22058d7865f
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-01 18:23:30 -0800

    Add text to loading spinner

--- src/components/general/loadingSpinner.js
+++ src/components/general/loadingSpinner.js
@@ -1,7 +1,13 @@
 import { Spinner } from "react-bootstrap";
 
 const LoadingSpinner = () => {
-  return <Spinner animation="border" variant="secondary" />;
+  return (
+    <div>
+      <h4>LOADING</h4>
+      <Spinner animation="border" variant="secondary" />
+      <h4>PLEASE STAND BY</h4>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
