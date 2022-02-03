f34352f4a8a9f0e91f96cd38a1691c53ac21a813
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-03 00:48:44 -0800

    Rough out quest details page

--- src/containers/pages/questDetailPage.js
+++ src/containers/pages/questDetailPage.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
+import { ListGroup } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { loadQuest } from "../../actions/questActions";
+import CellTable from "../../components/cells/cellTable";
 import LoadingSpinner from "../../components/general/loadingSpinner";
 import QuestTable from "../../components/quests/questTable";
 
@@ -16,10 +18,22 @@ const QuestDetailPage = (routerProps) => {
   }, []);
 
   if (quest) {
+    console.log(quest);
     return (
       <div>
         quest detail page
         <h2>{quest.title}</h2>
+        <ListGroup>
+          <ListGroup.Item>Title: {quest.title}</ListGroup.Item>
+          <ListGroup.Item>Summary: {quest.summary}</ListGroup.Item>
+          <ListGroup.Item>Details: {quest.details}</ListGroup.Item>
+          <ListGroup.Item>Category: {quest.category}</ListGroup.Item>
+          <ListGroup.Item>Wiki Link: {quest.wiki_link}</ListGroup.Item>
+          <ListGroup.Item>
+            <h4>Linked Cells:</h4>
+            <CellTable cells={quest.cells} />
+          </ListGroup.Item>
+        </ListGroup>
       </div>
     );
   } else {
