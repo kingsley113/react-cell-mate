4134d6b74f4f63025b9b1a991a82442c06ff0da8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-03 23:56:13 -0800

    Update render error function

--- src/helpers/renderErrors.js
+++ src/helpers/renderErrors.js
@@ -1,7 +1,15 @@
 const renderErrors = (errors) => {
   console.log(errors);
-
-  // alert(json.errors[0]);
+  let list = [];
+  for (const key in errors) {
+    console.log(key);
+    if (key === "name") {
+      list.push(`${key} ${errors[key]}`);
+    } else {
+      list.push(errors[key]);
+    }
+  }
+  alert(list);
 };
 
 export default renderErrors;
