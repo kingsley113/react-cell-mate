0786d864de485f99fc5b75a7833c3b4c7a6f5389
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-06 14:19:21 -0800

    Implement map color selector component

---
+++ src/components/cellMap/colorModeSelector.js
@@ -0,0 +1,35 @@
+import { Form } from "react-bootstrap";
+
+const ColorModeSelector = ({ getModeInput }) => {
+  const handleOnChange = (event) => {
+    getModeInput(event.target.id);
+  };
+
+  return (
+    <Form onChange={(e) => handleOnChange(e)}>
+      <Form.Check
+        inline
+        label="Cell Color"
+        name="color-selector-radio"
+        id="cell_color"
+        type="radio"
+      />
+      <Form.Check
+        inline
+        label="User Color"
+        name="color-selector-radio"
+        id="user_color"
+        type="radio"
+      />
+      <Form.Check
+        inline
+        label="Cell Progress"
+        name="color-selector-radio"
+        id="cell_progress"
+        type="radio"
+      />
+    </Form>
+  );
+};
+
+export default ColorModeSelector;
--- src/containers/pages/cellMapPage.js
+++ src/containers/pages/cellMapPage.js
@@ -1,17 +1,33 @@
 import React, { Component, useState } from "react";
+import { useSelector } from "react-redux";
 import CellMap from "../../components/cellMap/cellMap";
+import ColorModeSelector from "../../components/cellMap/colorModeSelector";
 
 const CellMapPage = (props) => {
-	const [colorMode, setColorMode] = useState("cell-color")
+  const [colorMode, setColorMode] = useState("cell_color");
+  const cells = useSelector((state) => state.cells.allCells);
+
+  const renderColorMode = () => {
+    switch (colorMode) {
+      case "cell_color":
+        return "Cell Color";
+      case "user_color":
+        return "User Color";
+      case "cell_progress":
+        return "Cell % Complete";
+      default:
+        return;
+    }
+  };
 
   return (
     <div>
-      this will be the cell map page
-			
-      <CellMap colorMode={}/>
+      <h2>Cell Map - {renderColorMode()}</h2>
+      <ColorModeSelector getModeInput={setColorMode} />
+      <CellMap colorMode={colorMode} cells={cells} />
     </div>
   );
-  // TODO: will need a header, selector for color mode, and the map, and show color scale and user legend when on the respective modes
+  // TODO: will need a header, and the map, and show color scale and user legend when on the respective modes
 };
 
 export default CellMapPage;
