4b83a10bba89d2c9c0333968e0f50890708d2bcf
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-05 17:36:47 -0800

    add update quest and add quest to reducer

--- src/reducers/questsReducer.js
+++ src/reducers/questsReducer.js
@@ -1,13 +1,17 @@
 function quests(state = { quests: [] }, action) {
   switch (action.type) {
     case "ADD_QUEST":
-    // TODO:
+      // console.log("add quest action: ", action.quest);
+      return { allQuests: [...state.allQuests, action.quest] };
     case "LOAD_QUEST":
       return { currentQuest: action.quest };
     case "LOAD_QUESTS":
       return { allQuests: action.quests };
     case "EDIT_QUEST":
-    // TODO:
+      const updatedQuests = state.allQuests.map((quest) => {
+        return quest.id === action.quest.id ? action.quest : quest;
+      });
+      return { ...state, allQuests: updatedQuests };
     case "DELETE_QUEST":
     // TODO:
     default:
