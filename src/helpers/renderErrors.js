adb0f95785353288477fe192ce75ef5287378040
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-05 17:46:04 -0800

    Update error render method to handle more error cases

--- src/helpers/renderErrors.js
+++ src/helpers/renderErrors.js
@@ -1,12 +1,23 @@
 const renderErrors = (errors) => {
-  console.log(errors);
+  // console.log(errors);
   let list = [];
   for (const key in errors) {
-    console.log(key);
-    if (key === "name") {
-      list.push(`${key} ${errors[key]}`);
-    } else {
-      list.push(errors[key]);
+    // console.log(key);
+    // if (key === "name") {
+    //   list.push(`${key} ${errors[key]}`);
+    // } else {
+    //   list.push(errors[key]);
+    // }
+    switch (key) {
+      case "name":
+        list.push(`${key} ${errors[key]}`);
+        break;
+      case "title":
+        list.push(`${key} ${errors[key]}`);
+        break;
+      default:
+        list.push(errors[key]);
+        break;
     }
   }
   alert(list);
