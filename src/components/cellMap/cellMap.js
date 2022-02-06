e6ebbfbcff6419cf6367fabcea9fd2b03277c77d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-06 15:19:05 -0800

    Building out cell map boxes

--- src/components/cellMap/cellMap.js
+++ src/components/cellMap/cellMap.js
@@ -1,5 +1,54 @@
+import { OverlayTrigger, Popover } from "react-bootstrap";
+
 const CellMap = ({ cells, colorMode }) => {
-  return <div id="cell-grid-container"></div>;
+  const renderCells = (cells, mode) => {
+    let cellObjects = [];
+
+    const cellInfoPanel = (cell) => {
+      return (
+        <Popover id="popover-basic">
+          <Popover.Header as="h3">{cell.name}</Popover.Header>
+          <Popover.Body>
+            X: {cell.ck_coordinate_x}
+            Y: {cell.ck_coordinate_y}
+            Color: {cell.color}
+          </Popover.Body>
+        </Popover>
+      );
+    };
+
+    if (cells) {
+      for (const cell of cells) {
+        // console.log(
+        //   cell.name,
+        //   " X: ",
+        //   cell.coordinate_x,
+        //   "Y: ",
+        //   cell.coordinate_y
+        // );
+        cellObjects.push(
+          <OverlayTrigger
+            key={cell.id}
+            trigger={["hover", "focus"]}
+            placement="top"
+            overlay={cellInfoPanel(cell)}
+          >
+            <div
+              className="cell-block"
+              style={{
+                gridColumnStart: cell.coordinate_x,
+                gridRowStart: cell.coordinate_y,
+                backgroundColor: cell.color,
+              }}
+            ></div>
+          </OverlayTrigger>
+        );
+      }
+      return cellObjects;
+    }
+  };
+
+  return <div id="cell-grid-container">{renderCells(cells, colorMode)}</div>;
 };
 
 export default CellMap;
