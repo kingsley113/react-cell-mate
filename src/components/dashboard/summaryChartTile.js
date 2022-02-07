a2bae93a76bdd78fa248d64cede2ec46876501c5
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-06 18:46:52 -0800

    Building out summary chart

--- src/components/dashboard/summaryChartTile.js
+++ src/components/dashboard/summaryChartTile.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import Chart from "react-google-charts";
+import { useSelector } from "react-redux";
 
-class SummaryChartTile extends Component {
-  render() {
-    return (
-      <div className="dashboard-tile" id="summary-chart-tile">
-        {/* TODO: Chart tile here */}
-      </div>
-    );
-  }
-}
+const SummaryChartTile = () => {
+  const cells = useSelector((state) => state.cells.allCells);
+  return (
+    <div className="dashboard-tile" id="summary-chart-tile">
+      {/* TODO: Chart tile here */}
+      <Chart
+        chartType="Histogram"
+        data={"TODO:"}
+        width="200px"
+        height="200px"
+        legendToggle
+      />
+    </div>
+  );
+};
 
 export default SummaryChartTile;
