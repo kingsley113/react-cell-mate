d98591020e673d0981b8d76a89f0816856136898
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-06 19:58:39 -0800

    Add shortcut buttons to dashboard

--- src/components/dashboard/mapShortcutTile.js
+++ src/components/dashboard/mapShortcutTile.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
-class MapShortcutTile extends Component {
-  render() {
-    return (
-      <div className="dashboard-tile" id="map-shortcut-tile">
-        // TODO: Map shortcut here
-      </div>
-    );
-  }
-}
+const MapShortcutTile = () => {
+  const history = useHistory();
+  return (
+    <div className="dashboard-tile" id="map-shortcut-tile">
+      <Button variant="primary" onClick={() => history.push(`/map`)}>
+        Cell Map
+      </Button>
+      <Button variant="primary" onClick={() => history.push(`/cells`)}>
+        Cell Index
+      </Button>
+      <Button variant="primary" onClick={() => history.push(`/cells/new`)}>
+        New Cell
+      </Button>
+    </div>
+  );
+};
 
 export default MapShortcutTile;
