8b3965fce5b9b39f67e539cbbd20599a0d6e8ee3
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-08 00:59:55 -0800

    create load current user action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -18,3 +18,26 @@ export const loadUsers = () => {
       });
   };
 };
+
+export const loadCurrentUser = () => {
+  return (dispatch) => {
+    const configurationObject = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("cellMateJWT")}`,
+      },
+    };
+    fetch(`${apiURL}/api/v1/profile`, configurationObject)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        // console.log(json);
+        localStorage.setItem("cellMateCurrentUser", JSON.stringify(json));
+        dispatch({ type: "SET_CURRENT_USER", currentUser: json });
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+  };
+};
