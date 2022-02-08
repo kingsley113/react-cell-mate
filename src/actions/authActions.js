78336fc04abbbf336de4e9520fbe430b17e95852
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-07 22:12:31 -0800

    add auth action page

---
+++ src/actions/authActions.js
@@ -0,0 +1,33 @@
+import { apiURL } from "./fetchConfig";
+
+export const authenticateDiscord = () => {
+  return (dispatch) => {
+    const configurationObject = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("cellMateJWT")}`,
+      },
+    };
+    fetch(`${apiURL}/api/v1/authenticate`, configurationObject)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.jwt && data.user) {
+          localStorage.setItem("cellmate_jwt", data.jwt);
+          localStorage.setItem(
+            "cellmate_current_user",
+            JSON.stringify(data.user)
+          );
+          dispatch({ type: "SET_USER", user: data.user });
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    localStorage.removeItem("cellmate_jwt");
+    localStorage.removeItem("cellmate_current_user");
+    dispatch({ type: "LOGOUT" });
+  };
+};
