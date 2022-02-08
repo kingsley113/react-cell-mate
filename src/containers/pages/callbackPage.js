1f0ab7b86ef36e232101ac26542c88349d4cd73c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-08 01:00:52 -0800

    Update localstorage jwt name

--- src/containers/pages/callbackPage.js
+++ src/containers/pages/callbackPage.js
@@ -3,7 +3,7 @@ import { Redirect } from "react-router-dom";
 const CallbackPage = (routerProps) => {
   const jwt = routerProps.location.search.split("?token=")[1];
   if (jwt) {
-    localStorage.setItem("cellMate_jwt", jwt);
+    localStorage.setItem("cellMateJWT", jwt);
     return <Redirect to="/" />;
   } else {
     return <Redirect tp="/login" />;
