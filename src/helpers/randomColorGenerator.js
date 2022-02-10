bd16c6b29fbaf51b69a270729198867393c5d9a2
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-09 19:25:05 -0800

    fix edge case of color generator

--- src/helpers/randomColorGenerator.js
+++ src/helpers/randomColorGenerator.js
@@ -1,5 +1,10 @@
 const randomColorGenerator = () => {
-  const color = Math.floor(Math.random() * 16777215).toString(16);
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  if (color.length < 6) {
+    console.log(color);
+    color = randomColorGenerator();
+    color.shift();
+  }
   return `#${color}`;
 };
 
