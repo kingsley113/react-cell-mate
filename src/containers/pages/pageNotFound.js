e4357e5c91caa607c38227c04222ec331a6439d8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-11 00:01:25 -0800

    Add loading background image to 404 page

--- src/containers/pages/pageNotFound.js
+++ src/containers/pages/pageNotFound.js
@@ -5,7 +5,12 @@ const PageNotFound = () => {
     document.title = "Lost in the Wasteland";
   }, []);
 
-  return <h2>Looks like your lost in the wasteland, Page not found. 404, </h2>;
+  return (
+    <div>
+      <h2>Looks like your lost in the wasteland, Page not found. 404, </h2>{" "}
+      <img src="./Loading_screen01.jpeg" alt="fallout loading screen" />
+    </div>
+  );
   // TODO:
 };
 
