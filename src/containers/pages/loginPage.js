8c921471b770f8919b706b9279bf97e505acc6df
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-11 00:03:35 -0800

    Build out login page

--- src/containers/pages/loginPage.js
+++ src/containers/pages/loginPage.js
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
-import { Nav } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Form, Nav } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import RedirectIfLoggedIn from "../../components/auth/redirectIfLoggedIn";
 
 const LoginPage = () => {
@@ -7,12 +8,66 @@ const LoginPage = () => {
     document.title = "CellMate Login";
   }, []);
 
+  const history = useHistory();
+  const [gatewayPass, setGatewayPass] = useState("");
+  const gatewayAccessCode = process.env.REACT_APP_GATEWAY_ACCESS_CODE;
+
+  useEffect(() => {
+    sessionStorage.setItem("gateway-authenticated", false);
+  }, []);
+
+  const handleOnChange = (event) => {
+    setGatewayPass(event.target.value);
+  };
+
+  const handleOnClick = () => {
+    console.log(gatewayAccessCode);
+    gatewayPass === gatewayAccessCode
+      ? (window.location.href = "http://localhost:8000/auth/discord")
+      : alert("Incorrect Access Code");
+  };
+
   return (
     <div>
       <RedirectIfLoggedIn>
-        <Nav.Link href="http://localhost:8000/auth/discord">
-          Login with Discord
-        </Nav.Link>
+        <div id="page-main">
+          <div>
+            <img src="./cascadia_banner.png" alt="fallout cascadia banner" />
+          </div>
+          <div>
+            <img
+              src="./cell-mate-logo.svg"
+              alt="CellMate Logo"
+              className="cellmate-logo"
+            />
+          </div>
+          <div className="pagination margin-top-20">
+            <Form.Group
+              className="mb-3 gateway-password-field"
+              controlId="gateway-password"
+            >
+              <Form.Label>Enter Super Top Secret Access Code:</Form.Label>
+              <Form.Control
+                type="password"
+                value={gatewayPass}
+                placeholder="password"
+                onChange={(e) => handleOnChange(e)}
+                className="gateway-password-field"
+              ></Form.Control>
+            </Form.Group>
+          </div>
+          {/* <Nav.Link href="http://localhost:8000/auth/discord"> */}
+          <div>
+            <h3>Login with Discord</h3>
+            <img
+              src="./Discord-Logo+Wordmark-White.svg"
+              alt="Discord Logo"
+              onClick={() => handleOnClick()}
+              className="width-300"
+            />
+          </div>
+          {/* </Nav.Link> */}
+        </div>
       </RedirectIfLoggedIn>
     </div>
   );
