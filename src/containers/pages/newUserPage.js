d2b2ed081f3f44d635319d4281312e6828ee1c2d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-11 00:40:19 -0800

    Delete  unused new user page

--- src/containers/pages/newUserPage.js
+++
@@ -1,9 +0,0 @@
-import RedirectIfLoggedIn from "../../components/auth/redirectIfLoggedIn";
-
-const NewUserPage = () => {
-  return (
-    <RedirectIfLoggedIn>new user page TODO: new user form</RedirectIfLoggedIn>
-  );
-};
-
-export default NewUserPage;
