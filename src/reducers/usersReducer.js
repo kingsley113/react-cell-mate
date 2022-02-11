b4421d7ca3cfcd6cd1f26a03f66d887552afa77a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-11 00:03:45 -0800

    fix bug in user reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -2,7 +2,7 @@ function users(state = { users: [] }, action) {
   switch (action.type) {
     case "CREATE_USER":
     case "LOAD_USERS":
-      return { allUsers: action.users };
+      return { ...state, allUsers: action.users };
     case "EDIT_USER":
     case "DELETE_USER":
     case "SET_CURRENT_USER":
