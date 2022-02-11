af3d5db0b9ac1f991d354ecf009b2747351aa9bd
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-11 00:40:50 -0800

    Clean up unused comments and code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@
 import { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// TODO:Import pages here and add to router below
-//TODO: LoginPage Component
-//TODO: NewUserPage Component
-//TODO: PageContent Component
+
 import PageContent from "./containers/pageContent";
 import PageNotFound from "./containers/pages/pageNotFound";
 import LoginPage from "./containers/pages/loginPage";
-import NewUserPage from "./containers/pages/newUserPage";
 import CallbackPage from "./containers/pages/callbackPage";
 
 class App extends Component {
