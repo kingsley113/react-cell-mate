cd99b990f36e48fdcafde4070a033d1ff4f3e4f6
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-12 11:39:23 -0800

    Clean up unused comments and code

--- src/helpers/renderErrors.js
+++ src/helpers/renderErrors.js
@@ -1,13 +1,6 @@
 const renderErrors = (errors) => {
-  // console.log(errors);
   let list = [];
   for (const key in errors) {
-    // console.log(key);
-    // if (key === "name") {
-    //   list.push(`${key} ${errors[key]}`);
-    // } else {
-    //   list.push(errors[key]);
-    // }
     switch (key) {
       case "name":
         list.push(`${key} ${errors[key]}`);
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,10 +1,7 @@
 function users(state = { users: [] }, action) {
   switch (action.type) {
-    case "CREATE_USER":
     case "LOAD_USERS":
       return { ...state, allUsers: action.users };
-    case "EDIT_USER":
-    case "DELETE_USER":
     case "SET_CURRENT_USER":
       return { ...state, currentUser: action.currentUser };
     case "LOGOUT":
