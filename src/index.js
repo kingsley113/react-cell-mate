cad299cc9bbb7ec6dea0072841e54be2a4b090e2
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-17 11:40:21 -0800

    Update createStore & composedEnhancer method

--- src/index.js
+++ src/index.js
@@ -10,10 +10,16 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers/index"; //TODO: make this reducer index
 import { Provider } from "react-redux";
 
-const composedEnhancer = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+let composedEnhancer;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+  composedEnhancer = compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+} else {
+  composedEnhancer = compose(applyMiddleware(thunk));
+}
 
 let store = createStore(rootReducer, composedEnhancer);
 
